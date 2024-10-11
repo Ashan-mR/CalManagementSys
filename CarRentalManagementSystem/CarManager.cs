2c98a7c476810fc88833d6b378576f4e495f97f1
Ravi <Ravi@DESKTOP-PMG59R1>
2024-10-11 09:18:15 +0530

    Crud

---
+++ CarRentalManagementSystem/CarManager.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarRentalManagementSystem
+{
+    public class CarManager
+    {
+        private List<Car> cars=new List<Car>();
+        private int carID = 1;
+
+        public void CreateCar()
+        {
+
+        }
+        public void ReadCars()
+        {
+
+        }
+        public void UpdateCars()
+        {
+
+        }
+        public void DeleteCars()
+        {
+
+        }
+
+    }
+}
