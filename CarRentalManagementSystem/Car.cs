647da58d2677ae7824ab8b0a55b11015c2bee975
Ravi <Ravi@DESKTOP-PMG59R1>
2024-10-11 08:31:08 +0530

    Initial commit

---
+++ CarRentalManagementSystem/Car.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarRentalManagementSystem
+{
+    public class Car
+    {
+  
+
+        public int carId { get; set; }
+        public string Carname { get; set; }
+        public string Model { get; set; }
+        public decimal RentPrice { get; set; }
+
+        public Car(int carId, string carname, string model, decimal rentPrice)
+        {
+            this.carId = carId;
+            Carname = carname;
+            Model = model;
+            RentPrice = rentPrice;
+        }
+
+    }
+}
